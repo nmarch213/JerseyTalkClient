fc43e6b8a96a34cbcf491339b05fdfe4eb271cc5
Nicholas March <n.march213@gmail.com>
2017-05-22 06:19:30 -0500

    Refactored the webpack, also added redux and form

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,26 @@
-const path = require('path');
-
-var DIST_DIR = path.resolve(__dirname, "dist");
-var SRC_DIR = path.resolve(__dirname, "src");
-
-var config = {
-  entry: SRC_DIR + "/app/index.js",
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
   output: {
-    path: DIST_DIR + "/app",
-    filename: "bundle.js",
-    publicPath: "/app/"
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
   },
   module: {
-    loaders: [
-      {
-        test: /\.js?/,
-        include: SRC_DIR,
-        loader: "babel-loader",
-        query: {
-          presets: ["react", "es2015", "stage-2"]
-        }
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015', 'stage-2']
       }
-    ]
+    }]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
   }
 };
-
-module.exports = config;
