ae62d911389da2ee1d515e12ae499d2fe4bdb7a0
Nicholas March <n.march213@gmail.com>
2017-05-29 08:02:11 -0500

    Refactor location of components

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 
 import NavBar from './layout/nav';
+import UniversityReviewForm from './universityReviewForm/UniversityReviewForm';
+import showResults from './universityReviewForm/showResults';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-
+        <NavBar />
+        <UniversityReviewForm onSubmit={showResults}/>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/app';
 import store from './reducers';
-import NavBar from './components/layout/nav';
-import UniversityReviewForm from './components/universityReviewForm/UniversityReviewForm';
-import showResults from './components/universityReviewForm/showResults';
+
 
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
@@ -15,9 +13,7 @@ const createStoreWithMiddleware = applyMiddleware()(createStore);
 ReactDOM.render(
   <Provider store={store}>
     <div>
-      <NavBar />
       <App />
-      <UniversityReviewForm onSubmit={showResults}/>
     </div>
   </Provider>
   , document.querySelector('.container'));
