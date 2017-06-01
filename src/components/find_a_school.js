1783a2c9919bb52d9aea2e01993fec99fc98d2f0
Nicholas March <n.march213@gmail.com>
2017-06-01 09:13:22 -0500

    Refactor code to deal with proper navigation

---
+++ src/components/find_a_school.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+import UniversityReviewForm from './universityReviewForm/UniversityReviewForm';
+import showResults from './universityReviewForm/showResults';
+import InstitutionCard from '../containers/institution-card';
+
+class FindASchool extends Component {
+  render(){
+    return (
+      <div>
+        <UniversityReviewForm onSubmit={showResults}/>
+        <InstitutionCard  />
+      </div>
+    );
+  }
+}
+
+export default FindASchool;
--- src/index.js
+++ src/index.js
@@ -3,18 +3,26 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
 
+import FindASchool from './components/find_a_school';
+import NavBar from './components/layout/nav';
 
 
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <div>
-      <App />
-    </div>
+    <BrowserRouter>
+      <div>
+        <NavBar />
+        <Switch>
+          <Route path="/" component={FindASchool} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
