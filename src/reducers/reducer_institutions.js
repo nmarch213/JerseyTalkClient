0aefbc76d05c4617f9f9b7bfff125594977652ef
Nicholas March <n.march213@gmail.com>
2017-06-02 08:07:49 -0500

    Added FETCH_SCHOOL reducer

--- src/reducers/reducer_institutions.js
+++ src/reducers/reducer_institutions.js
@@ -1,10 +1,12 @@
-import { FETCH_CORE_INFORMATION } from '../actions/index';
+import { FETCH_CORE_INFORMATION, FETCH_SCHOOL } from '../actions/index';
 import _ from 'lodash';
 
 export default function(state = {}, action) {
   switch(action.type){
     case FETCH_CORE_INFORMATION:
       return _.mapKeys(action.payload.data, 'id');
+    case FETCH_SCHOOL:
+      return { ...state, [action.payload.data.id]: action.payload.data }; //key Interpolation
     default:
       return state;
   }
