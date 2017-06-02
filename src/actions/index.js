94cd4d139e3dc8644242c74e9878daf2a0241e96
Nicholas March <n.march213@gmail.com>
2017-06-02 08:01:36 -0500

    Added FETCH_SCHOOL Action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_CORE_INFORMATION = 'FETCH_CORE_INFORMATION';
+export const FETCH_SCHOOL = 'FETCH_SCHOOL'
 
 const ROOT_URL = "http://localhost:3000"
 
@@ -8,6 +9,14 @@ export function fetchCoreInformation(){
   const request = axios.get(`${ROOT_URL}/getUniversityCoreInfo`);
   return {
     type: FETCH_CORE_INFORMATION,
+    payload: request
+  };
+}
+
+export function fetchSchool(id){
+  const request = axios.get(`${ROOT_URL}/getSchool/${id}`);
+  return {
+    type: FETCH_SCHOOL,
     payload: request
   };
 }
