2d2a80275c516b61e94fdee329d7c9b5eec612f4
Nicholas March <n.march213@gmail.com>
2017-06-08 20:04:17 -0500

    Able to display sport from school

--- src/components/universityReviewForm/UniversityReviewFormFirstPage.js
+++ src/components/universityReviewForm/UniversityReviewFormFirstPage.js
@@ -1,54 +1,48 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import validate from './validate';
 import renderField from './renderField';
 
+class UniversityReviewFormFirstPage extends Component {
 
-const UniversityReviewFormFirstPage = (props) => {
-  const { handleSubmit } = props;
-  console.log(props);
-  return (
-    <div className="container-fluid">
-      <h1>A little about you</h1>
-      <p>None of this information will be shown unless explicitly asked</p>
-      <form onSubmit={handleSubmit}>
-        <Field
-          name="athleteName"
-          type="text"
-          component={renderField}
-          label="Who are you?"
-        />
-        <Field
-          name="athleteEmail"
-          type="email"
-          component={renderField}
-          label="Whats your email?"
-        />
-        <div className="form-group">
-          <label htmlFor="athleteSport">What Sport did you play?</label>
+  renderSports() {
+    return _.map(this.props.school.sports, sport => (
+      <option key={sport.ncaaId} value={sport.index}>{sport.name}</option>
+    ));
+  }
+  render() {
+    const { school } = this.props;
+    const { handleSubmit } = this.props;
+
+    const renderError = ({ meta: { touched, error } }) => touched && error ?
+      <span>{error}</span> : false;
+
+    if (!school) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <div className="container-fluid">
+        <form onSubmit={handleSubmit}>
+          <h1>Please Select The Sport You Played</h1>
+
           <Field
-            name="athleteSport"
-            component="select"
             className="form-control"
+            id="sport"
+            name="sport"
+            component="select"
           >
-            <option />
-            <option value="mBball">Mens Basketball</option>
-            <option value="wBball">Womens Basketball</option>
-            <option value="fball">Football</option>
-            <option value="bball">Baseball</option>
-            <option value="sball">Softball</option>
-            <option value="mSoccer">Mens Soccer</option>
-            <option value="wSoccer">Womens Soccer</option>
-            <option value="vball">Womens Volleyball</option>
+            {this.renderSports()}
           </Field>
-        </div>
-        <div>
-          <button type="submit" className="next">Next</button>
-        </div>
-      </form>
-    </div>
-  );
-};
+          <div>
+            <button type="submit" className="next">Next</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
 
 export default reduxForm({
   form: 'UniversityReviewForm',              // <------ same form name
