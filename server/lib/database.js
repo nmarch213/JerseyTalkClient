d02bbef1854fdd7d0dbf1d26687b1f41546f5285
Nicholas March <n.march213@gmail.com>
2017-06-14 22:48:12 -0500

    Edits to Navbar to match Mockup

--- server/lib/database.js
+++ server/lib/database.js
@@ -4,22 +4,22 @@ const mongoose = require('mongoose');
 
 module.exports = {
 
-    populateDBSettings: function(app) {
+    populateDBSettings: function (app) {
       var options = {
           server: {
              socketOptions: { keepAlive: 1 }
           }
       };
-      switch(app.get('env')){
-          case 'development':
-              const credentials = require('../credentials.js');
-              mongoose.connect(credentials.mongo.development.connectionString, options);
-              break;
-          case 'production':
-              mongoose.connect(process.env.MongoLab, options);
-              break;
-          default:
-              throw new Error('Unknown execution environment: ' + app.get('env'));
+      switch (app.get('env')) {
+        case 'development':
+          const credentials = require('../credentials.js');
+          mongoose.connect(credentials.mongo.development.connectionString, options);
+          break;
+        case 'production':
+          mongoose.connect(process.env.MongoLab, options);
+          break;
+        default:
+          throw new Error('Unknown execution environment: ' + app.get('env'));
       }
 
     }
--- src/components/layout/nav.js
+++ src/components/layout/nav.js
@@ -19,71 +19,62 @@ export default class NavBar extends Component {
 
     return (
       <nav className="navbar navbar-static-top navbar-toggleable-md navbar-inverse" id="navbarMain">
-        <button
-          className="navbar-toggler navbar-toggler-right"
-          data-toggle="collapse"
-          data-target="#navbarContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <img
-          className="navbar-brand"
-          src="/src/images/logo/Jerseytalk-logo-half-jerseyWHITE.png"
-          alt="JerseyTalk Logo"
-          id="logo"
-        />
+        <div className="container">
+          <button
+            className="navbar-toggler navbar-toggler-right"
+            data-toggle="collapse"
+            data-target="#navbarContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <img
+            className="navbar-brand"
+            src="/src/images/logo/Jerseytalk-logo-half-jerseyWHITE.png"
+            alt="JerseyTalk Logo"
+            id="logo"
+          />
+          <div className="container">
+            <div className="collapse navbar-collapse" id="navbarContent">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item float-right">
+                  <NavLink
+                    to="/"
+                    className="nav-link"
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                <li className="nav-item float-right">
+                  <NavLink
+                    to="/review"
+                    className="nav-link"
+                  >
+                    Write Review
+                  </NavLink>
+                </li>
+                <li className="nav-item float-right">
+                  <NavLink
+                    to="/about-us"
+                    className="nav-link"
+                  >
+                    About
+                  </NavLink>
+                </li>
+                <li className="nav-item float-right">
+                  <NavLink
+                    to="/contact-us"
+                    className="nav-link"
+                  >
+                    Contact
+                  </NavLink>
 
-        <div className="collapse navbar-collapse" id="navbarContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <NavLink
-                to="/"
-                className="nav-link"
-              >
-                Home
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                to="/review"
-                className="nav-link"
-              >
-                Write Review
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                to="/ratings"
-                className="nav-link"
-              >
-                Ratings
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                to="/about-us"
-                className="nav-link"
-              >
-                About
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                to="/contact-us"
-                className="nav-link"
-              >
-                Contact
-              </NavLink>
-
-            </li>
-          </ul>
-          <form className="form-inline my-2 my-lg-0">
-            <input className="form-control mr-sm-2" type="text" placeholder="Find Your School" />
-            <button className="btn btn-outline-secondary my-2 my-sm-0" type="submit" >Search</button>
-          </form>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </nav>
     );
