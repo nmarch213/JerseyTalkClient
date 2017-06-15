35a54c9a08a4ef0990fb593c800c73edeb883245
Nicholas March <n.march213@gmail.com>
2017-06-15 03:52:25 -0500

    Navbar CSS updates

---
+++ src/components/home_jumbo.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+export default class Footer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapsed: true,
+    };
+  }
+
+  render() {
+    // const { location } = this.props;
+
+    return (
+      <div className="jumbotron text-center" id="jumboHome">
+        
+      </div>
+    );
+  }
+}
